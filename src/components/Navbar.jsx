ccd5e9ffc85ab590af61992e1286a45d267666a8
alphacalling <vikaspandey490@gmail.com>
2024-10-29 18:21:27 +0530

    css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="bg-blue-600 p-4 shadow-md sticky top-0">
-      <ul className="flex items-center justify-between max-w-7xl mx-auto px-4 text-white">
+      <ul className="flex items-center sm:justify-between justify-around max-w-7xl mx-auto px-4 text-white">
         <div className="flex space-x-8">
           <li>
             <Link to="/" className="hover:text-gray-300 text-lg font-medium">
@@ -36,7 +36,7 @@ function Navbar() {
             <li>
               <Link
                 to="/signup"
-                className="bg-white text-blue-600 hover:bg-gray-200 px-4 py-2 rounded-full font-semibold transition-all duration-300 ease-in-out"
+                className="bg-white text-blue-600 hover:bg-gray-200 px-4 py-2 mr-2 rounded-full font-semibold transition-all duration-300 ease-in-out"
               >
                 Signup
               </Link>
